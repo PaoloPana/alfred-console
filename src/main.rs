d97bb3491fb8d69ee53dc58e7fe0e0a881608295
Paolo Panarese <panarese.paolo@gmail.com>
2024-11-09 19:16:42 +0100

    added response_topics

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,20 @@ async fn main() -> Result<(), Error> {
             loop {
                 let mut topic = String::new();
                 let mut text = String::new();
+                let mut response_topics = String::new();
                 println!("Insert message to send to a specific topic (topic: text): ");
                 if scanf!("{}: {}", topic, text).is_err() { continue; }
+                println!("Insert responseTopics separated by whitespace (blank if none): ");
+                if scanf!("{}", response_topics).is_err() { continue; }
+                let response_topics = if response_topics.is_empty() {
+                    LinkedList::from([INPUT_TOPIC.to_string()])
+                } else {
+                    response_topics.split(' ').map(String::from).collect()
+                };
                 let message = Message {
                     text: text.clone(),
                     message_type: MessageType::Text,
-                    response_topics: LinkedList::from([INPUT_TOPIC.to_string()]),
+                    response_topics,
                     ..Message::default()
                 };
                 if publisher.send(topic.as_str(), &message).await.is_ok() {
