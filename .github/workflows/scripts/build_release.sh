682c21cec313e55b9a2c65b13f8b731f38ff7eaa
PaoloPana <panarese.paolo@gmail.com>
2024-12-31 19:01:00 +0100

    created CI/CD

---
+++ .github/workflows/scripts/build_release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xeu
+
+ARCH=${1}
+echo "Installing cross..."
+cargo install cross --git https://github.com/cross-rs/cross
+echo "Building for arch ${ARCH}..."
+cross build --release --target ${ARCH}-unknown-linux-gnu
+echo "Copying bin file..."
+OUT_FOLDER="alfred-console"
+BIN_FOLDER="target/${ARCH}-unknown-linux-gnu/release"
+mkdir $OUT_FOLDER
+cp $BIN_FOLDER/alfred-console $OUT_FOLDER/
+
+tar czf alfred-console_${ARCH}.tar.gz $OUT_FOLDER
---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [Unreleased] - yyyy-mm-dd
+
+### Added
+- Created module for interacting with Alfred ecosystem using terminal
+- Added CI/CD 
